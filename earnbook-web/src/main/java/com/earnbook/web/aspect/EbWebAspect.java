39a11563ceef0896639fd95027dc69940da54567
yash <datta.walake@gmail.com>
2014-01-11 02:09:45 -0500

    web aspect added

---
+++ earnbook-web/src/main/java/com/earnbook/web/aspect/EbWebAspect.java
@@ -0,0 +1,34 @@
+package com.earnbook.web.aspect;
+
+import com.earnbook.web.exception.EbException;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by yash on 1/4/14.
+ */
+@Aspect
+public class EbWebAspect {
+
+    @Before(value="(execution(* com.earnbook.ebs.client.delegate.service.*.*(..)))")
+    public void preProcessValidation(JoinPoint joinPoint) {
+        System.out.print("preProcessValidation in @Before EbWebAspect");
+
+    }
+
+    @AfterThrowing(value="(execution(* com.earnbook.web.controller.*.*(..)))", throwing ="result", argNames = "")
+      public String callOnThrowException(JoinPoint joinPoint, EbException result) throws EbException{
+        System.out.println(">>>>>>>>>> In callOnThrowException >>>>>>>>>>>>>>>>");
+
+        return "user does not exist on Web AOP";
+
+    }
+
+
+
+}
